80143ea27df11be4777c7e7a5afde23be6dcebb7
TuannAnhh <minhnhat24422@gmail.com>
2023-11-10 16:50:40 +0700

    edt link

---
+++ hotcoffee-html/admin/taikhoan/list.php
@@ -0,0 +1,47 @@
+<div class="row2">
+    <div class="row2 font_title">
+        <h1>DANH SÁCH TÀI KHOẢN</h1>
+    </div>
+    <div class="row2 form_content ">
+        <form action="#" method="POST">
+            <div class="row2 mb10 formds_loai">
+                <table>
+                    <tr>
+                        <th></th>
+                        <th>MÃ TÀI KHOẢN</th>
+                        <th>TÊN TÀI KHOẢN</th>
+                        <th>PASSWORD</th>
+                        <th>EMAIL</th>
+                        <th>ĐỊA CHỈ</th>
+                        <th>SỐ ĐIỆN THOẠI</th>
+                        <th>VAI TRÒ</th>
+                        <th></th>
+                    </tr>
+                    <?php 
+                        foreach($listtaikhoan as $taikhoan){
+                            extract($taikhoan);
+                            $xoatk = 'index.php?act=xoatk&id='.$id;
+
+                            echo ' <tr>
+                                    <td><input type="checkbox" name="" id=""></td>
+                                    <td>'.$id.'</td>
+                                    <td>'.$user.'</td>
+                                    <td>'.$pass.'</td>
+                                    <td>'.$email.'</td>
+                                    <td>'.$address.'</td>
+                                    <td>'.$tel.'</td>
+                                    <td>'.$role.'</td>
+                                    <td><a href="'.$xoatk.'"><input type="button" value="Xóa"></td></a>
+                                    </tr>';
+                        }
+                    ?> 
+                </table>
+            </div>
+            <div class="row mb10 ">
+                <input class="mr20" type="button" value="CHỌN TẤT CẢ">
+                <input  class="mr20" type="button" value="BỎ CHỌN TẤT CẢ">
+            </div>
+        </form>
+    </div>
+</div>
+</div> 
