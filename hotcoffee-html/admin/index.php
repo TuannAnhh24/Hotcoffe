38218a41eb3418e80e3ba82072abcd00e944e4bf
AlexBomx <vudangthao16320@gmail.com>
2023-11-10 22:33:37 +0700

    up

--- hotcoffee-html/admin/index.php
+++ hotcoffee-html/admin/index.php
@@ -3,6 +3,9 @@
     if (isset($_GET['act']) ){
         $act = $_GET['act'];
         switch ($act){ 
+            case 'sanpham':
+                include "sanpham/list.php";
+                break;
         }
     }else {
         include "home.php";
--- hotcoffee-html/admin/sanpham/list.php
+++ hotcoffee-html/admin/sanpham/list.php
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin.css">
-    <title>sản phẩm</title>
-</head>
-
-<body>
-    <div class="product-list">
-        <div class="product-item">
-            <img src="hotcoffee-html/images/123.jpg" alt="Coffee Product">
-            <h3>Cà Phê Robusta</h3>
-            <p>Giá: $5.99</p>
-            <p>Số Lượng Tồn Kho: 50</p>
-            <button class="edit-product">Chỉnh Sửa</button>
-            <button class="delete-product">Xóa</button>
-        </div>
-        <!-- Thêm các sản phẩm khác vào danh sách -->
-      
-        <div class="product-filter">
-            <label for="category-filter">Lọc theo Danh Mục:</label>
-            <select id="category-filter">
-                <option value="all">Tất Cả</option>
-                <option value="brewed">Cà Phê Pha</option>
-                <option value="espresso">Espresso</option>
-                <option value="beans">Hạt Cà Phê</option>
-            </select>
-
-            <input type="text" id="search-product" placeholder="Tìm kiếm...">
-            <button id="search-button">Tìm Kiếm</button>
-        </div>
-
-
-    </div>
-
-</body>
-
-</html>
-=======
 <div class="row2">
          <div class="row2 font_title">
           <h1>DANH SÁCH LOẠI SẢN PHẨM</h1>
@@ -62,13 +19,13 @@
            <div class="row2 mb10 formds_loai">
            <table>
             <tr>
-                <th></th>
-                <th>MÃ LOẠI</th>
-                <th>Tên Sản phẩm </th>
-                <th>Hình</th>
-                <th>Giá</th>
-                <th>Lượt xem</th>
-                <th></th>
+                <th>Tên sản phẩm</th>
+                <th>Giá gốc </th>
+                <th>Giá khuyến mãi</th>
+                <th>View</th>
+                <th>Số lượng</th>
+                <th> Mô tả</th>
+                <th>ẢNh</th>
             </tr>
             <?php 
                 foreach($listsanpham as $sanpham){
@@ -84,11 +41,14 @@
 
                     echo ' <tr>
                             <td><input type="checkbox" name="" id=""></td>
-                            <td>'.$id.'</td>
-                            <td>'.$name.'</td>
-                            <td>'.$hinh.'</td>
-                            <td>'.$price.'</td>
+                            <td>'.$name_sp.'</td>
+                            <td>'.$gia_goc.'</td>
+                            <td>'.$gia_km.'</td>
                             <td>'.$view.'</td>
+                            <td>'.$size.'</td>
+                            <td>'.$so_luong.'</td>
+                            <td>'.$mo_ta.'</td>
+                            <td>'.$img.'</td>
                             <td><a href="'.$suasp.'"><input type="button" value="Sửa"></a>   <a href="'.$xoasp.'"><input type="button" value="Xóa"></td></a>
                             </tr>';
                 }
@@ -104,4 +64,3 @@
          </div>
         </div>
     </div> 
->>>>>>> bffc6ef77877d00b0064566f1939b4c65e8ce537
