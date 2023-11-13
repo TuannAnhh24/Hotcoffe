62da29c0f3dca790113d02015b6c1399e50e48df
AlexBomx <vudangthao16320@gmail.com>
2023-11-13 17:40:30 +0700

    code

--- hotcoffee/admin/index.php
+++ hotcoffee/admin/index.php
@@ -7,17 +7,17 @@
     if (isset($_GET['act']) ){
         $act = $_GET['act'];
         switch ($act){ 
-            // case 'adddm':
-            //     // kiểm tra xem người dùng có click vào nút add hay không 
-            //     if(isset($_POST['themmoi']) && ($_POST['themmoi'])){
-            //         $tenDanhmuc = $_POST['tenDanhmuc'];
-            //         // thêm cột thêm danh_muc(name,???) và VALUES('$tenDanhmuc'. '???' )
-            //         $sql= "INSERT INTO danh_muc(name) VALUES('$tenDanhmuc') ";
-            //         pdo_execute($sql);
-            //         $thongbao = "Thêm thành công";
-            //     }
-            //     include "danhmuc/add.php";
-            //     break;
+            case 'adddm':
+                // kiểm tra xem người dùng có click vào nút add hay không 
+                if(isset($_POST['themmoi']) && ($_POST['themmoi'])){
+                    $tenDanhmuc = $_POST['tenDanhmuc'];
+                    // thêm cột thêm danh_muc(name,???) và VALUES('$tenDanhmuc'. '???' )
+                    $sql= "INSERT INTO danh_muc(name) VALUES('$tenDanhmuc') ";
+                    pdo_execute($sql);
+                    $thongbao = "Thêm thành công";
+                }
+                include "danhmuc/add.php";
+                break;
             case 'listdm':
                 $sql = "SELECT * FROM danh_muc order by name ";
                 $listdanhmuc = pdo_query($sql);
@@ -41,7 +41,7 @@
                     }else{
                         echo "up ảnh bị lỗi";
                     }
-                   var_dump(test($nameSp,$giaGoc,$giaKm,$moTa,$img,$idDm));
+            
                     $thongbao ="Thêm thành công";     
                 }
                 $listdanhmuc = loadall_danhmuc();
