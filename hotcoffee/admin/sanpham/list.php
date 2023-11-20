6832442f2c134113340d69de11f3e4a70983fce5
TuannAnhh <minhnhat24422@gmail.com>
2023-11-20 16:44:15 +0700

    phantrang admin

--- hotcoffee/admin/sanpham/list.php
+++ hotcoffee/admin/sanpham/list.php
@@ -1,3 +1,40 @@
+<!-- phân trang  -->
+<?php
+    // // Số lượng sản phẩm trên mỗi trang
+    // $products_per_page = 5;
+    // // Lấy số trang hiện tại từ URL, nếu không có thì mặc định là 1
+    // $page = isset($_GET['trang']) ? (int)$_GET['trang'] : 1;
+    // // Tính vị trí sản phẩm đầu tiên trên trang
+    // $first_product = ($page - 1) * $products_per_page;
+    // $sql = "SELECT * FROM san_pham LIMIT $first_product, $products_per_page";
+    // // Thực hiện một câu truy vấn riêng để lấy tổng số sản phẩm
+    // $sql_total = "SELECT COUNT(*) as total FROM san_pham";
+    // $result_total = pdo_query_one($sql_total);
+    // $total_products = $result_total['total'];
+    // // Tính toán số lượng trang
+    // $trang = ceil($total_products / $products_per_page);
+
+    // Số lượng sản phẩm trên mỗi trang
+    $products_per_page = 5;
+    // Lấy số trang hiện tại từ URL, nếu không có thì mặc định là 1
+    if(isset($_GET['trang'])){
+        $page = $_GET['trang'];
+    }else{
+        $page = '';
+    }
+    if($page == '' || $page == 1){
+        $begin = 0;
+    }else {
+        $begin = ($page*3)-3;
+    }
+    $sql = "SELECT * FROM san_pham LIMIT $begin, $products_per_page";
+    // Thực hiện một câu truy vấn riêng để lấy tổng số sản phẩm
+    $sql_total = "SELECT COUNT(*) as total FROM san_pham";
+    $result_total = pdo_query_one($sql_total);
+    $total_products = $result_total['total'];
+    // Tính toán số lượng trang
+    $trang = ceil($total_products / $products_per_page);
+?>
 <div class="row2">
          <div class="row2 font_title">
           <h1>DANH SÁCH LOẠI SẢN PHẨM</h1>
@@ -29,6 +66,8 @@
                 <th>Chức năng</th>
             </tr>
             <?php 
+            $sql = "SELECT * FROM san_pham LIMIT $begin, $products_per_page";
+            $listsanpham = pdo_query($sql);
                 foreach($listsanpham as $sanpham){
                     extract($sanpham);
                     $suasp = 'index.php?act=suasp&id='.$id_sp;
@@ -62,17 +101,15 @@
                 <a href="index.php?act=addsp"> <input  class="mr20" type="button" value="NHẬP THÊM"></a>
            </div>
           </form>
-          <!-- phân trang  -->
-            <?php 
-                $sql = "SELECT * FROM san_pham";
-                $row_count = pdo_query($sql);
-                
-            ?>
             <nav id="pagination" class="pagination_wrap pagination_pages">
-                <span class="pager_current active ">1</span>
-                <a href="#" class="">2</a>
-                <a href="#" class="pager_next"></a>
-                <a href="#" class="pager_last"></a>
+                <?php 
+                    for ($i = 1; $i<= $trang; $i++ ){
+                        echo '<a class="pager_current active" href="index.php?trang='.$i.'">'.$i.'</a>';
+                    }
+                 ?>
+                <!-- <a href="#" class="">2</a> -->
+                <!-- <a href="#" class="pager_next"></a>
+                <a href="#" class="pager_last"></a> -->
             </nav>
          </div>
         </div>
